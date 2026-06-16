63c3cf15525158f283bdd238077e120b224ae1b2
priyabattula211 <priyabattula211@gmail.com>
2026-06-16 13:19:54 +0530

    Add files via upload

---
+++ backend/src/utils/jwt.js
@@ -0,0 +1,10 @@
+import jwt from 'jsonwebtoken';
+import { config } from '../config.js';
+
+export function signToken(payload) {
+  return jwt.sign(payload, config.jwtSecret, { expiresIn: '7d' });
+}
+
+export function verifyToken(token) {
+  return jwt.verify(token, config.jwtSecret);
+}
